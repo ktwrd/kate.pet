beb44328f97409602ca80a5f73215520023bd4fb
kate <kate@dariox.club>
2024-01-12 19:55:20 +0800

    Add more blog posts

---
+++ blog_posts/20240112-twitter-locked.php
@@ -0,0 +1,11 @@
+<?php
+
+$generate_blog_post = array(
+    'subject' => 'Twitter Account Locked (again)',
+    'description' => '',
+    'created_at' => mktime(19, 48, 59, 01, 12, 2024),
+    'hide_state' => 0,
+    'tags' => array()
+);
+
+?>

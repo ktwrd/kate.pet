0f866d793c43c257c8bed92b8a643f048ae5808d
kate <kate@dariox.club>
2024-02-22 16:29:35 +0800

    Update 20240112-twitter-locked

--- blog_posts/20240112-twitter-locked.php
+++ blog_posts/20240112-twitter-locked.php
@@ -4,6 +4,7 @@ $generate_blog_post = array(
     'subject' => 'Twitter Account Locked (again)',
     'description' => '',
     'created_at' => mktime(19, 48, 59, 01, 12, 2024),
+    'updated_at' => mktime(20, 00, 00, 01, 19, 2024),
     'hide_state' => 0,
     'tags' => array()
 );

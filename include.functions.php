7859654561e83fa3520270d26f4504aa8d6f7170
kate <kate@dariox.club>
2024-03-25 13:52:41 +0800

    Add support to filter by tags

--- include.functions.php
+++ include.functions.php
@@ -23,85 +23,6 @@ function displayBlogPostToUser($post)
     }
     return false;
 }
-function retrieveBlogPost($postId)
-{
-    $post = array();
-    if ($postId != null)
-    {
-        $postId = basename($postId);
-        include(K_WEB_ROOT . "/blog_posts/$postId.php");
-        if ($generate_blog_post != null)
-        {
-            $res = $generate_blog_post;
-
-            $post['text'] = formatMarkdown(file_get_contents(K_WEB_ROOT. "/blog_posts/$postId.md"));
-            $post['subject'] = $res['subject'];
-            $post['description'] = isset($res['description']) ? $res['description'] : "";
-            $post['created_at'] = $res['created_at'];
-            if (isset($res['updated_at']))
-            {
-                $post['updated_at'] = $res['updated_at'];
-            }
-            // 0: show
-            // 1: pretend it doesn't exist
-            // 2: hide in listing
-            $post['hide_state'] = isset($res['hide_state']) ? $res['hide_state'] : 0;
-            if (isset($post['created_at']))
-            {
-                $post['created_at_f'] = date('F j, Y', $post['created_at']);
-                $post['created_at_fl'] = date('c', $post['created_at']);
-            }
-            if (isset($post['updated_at']))
-            {
-                $post['updated_at_f'] = date('F j, Y', $post['updated_at']);
-                $post['updated_at_fl'] = date('c', $post['updated_at']);
-            }
-
-            if (isset($res['meta']))
-            {
-                if (isset($_META))
-                {
-                    // concat post meta before current meta to override things
-                    $_META = array_merge($res['meta'], $_META);
-                }
-                else
-                {
-                    $_META = $res['meta'];
-                }
-            }
-        }
-    }
-    return $post;
-}
-
-function getAllBlogPosts()
-{
-    $files = scandir(K_WEB_ROOT . "/blog_posts/");
-    $result = array();
-    foreach ($files as $f)
-    {
-        if (str_ends_with($f, '.php'))
-        {
-            $pn = preg_replace('/\.\w+$/', '', $f);
-            $p = retrieveBlogPost($pn);
-            if ($p != null)
-            {
-                $pm = array_merge($p,
-                array(
-                    'id' => $pn
-                ));
-                array_push($result, $pm);
-            }
-        }
-    }
-    usort( $result, function( $a, $b )
-    {
-        if ( $a['created_at'] === $b['created_at'] ) return 0;
-        if ( strpos( $b['created_at'], '0000' ) !== false ) return 1;
-        return ( $a['created_at'] < $b['created_at'] ) ? 1 : -1;
-    });
-    return $result;
-}
 
 function generate_navbar_data() {
     $data = array(
@@ -138,5 +59,5 @@ function generate_navbar_data() {
     );
     return $data;
 }
-
+require_once(K_WEB_ROOT . '/include.blog.php');
 ?>
--- pages/blog.php
+++ pages/blog.php
@@ -1,6 +1,14 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/include.php');
 
+$defaultTitle = "kate's blog";
+$defaultDescription = "sometimes rambles, sometimes important updates. you'll never know!";
+
+if (isset($_REQUEST['tag'])) {
+    $smarty->assign('filterTag', $_REQUEST['tag']);
+} else {
+    $smarty->assign('filterTag', NULL);
+}
 
 $postHideState = 1;
 $showPostListing = False;
@@ -11,33 +19,60 @@ if (isset($_REQUEST['i']))
     $postContent = retrieveBlogPost(basename($_REQUEST['i']));
     if ($postContent == null)
     {
+        http_response_code(301);
         header('Location: /p/blog');
     }
     else
     {
         $postHideState = isset($postContent['hide_state']) ? $postContent['hide_state'] : 2;
         $smarty->assign('post', $postContent);
-        $smarty->assign('postTitle', $postContent['subject'] . ' - kate\'s blog');
-        $smarty->assign('title', $postContent['subject'] . ' - kate\'s blog');
+        $subject = '';
+        if (isset($postContent['subject'])) {
+            $subject = $postContent['subject'];
+        }
+        $smarty->assign('postTitle', $subject . ' - ' . $defaultTitle);
         $smarty->assign('postDescription', $postContent['description']);
-        $smarty->assign('description', $postContent['description']);
     }
 
     if (displayBlogPostToUser($postContent))
     {
-        $smarty->assign('title', $postContent['subject'] . ' - kate\'s blog');
-        $smarty->assign('description', $postContent['description']);
+        if (!isset($postContent['subject'])) {
+            $smarty->assign('title', $defaultTitle);
+        } else {
+            $smarty->assign('title', $postContent['subject'] . ' - ' . $defaultTitle);
+        }
+        if (isset($postContent['description']))
+        {
+            $smarty->assign('description', $postContent['description']);
+        }
+        else
+        {
+            $smarty->assign('description', $defaultDescription);
+        }
+    }
+    else
+    {
+        http_response_code(301);
+        header('Location: /p/blog'); 
     }
 }
 else
 {
     $showPostListing = True;
-    $smarty->assign('postArray', getAllBlogPosts());
-    $smarty->assign('title', 'kate\'s blog');
-    $smarty->assign('description', 'sometimes rambles, sometimes important updates. you\'ll never know!');
+    $postArray = getAllBlogPosts();
+    $postTagArray = array();
+    foreach ($postArray as $p) {
+        foreach ($p['tags'] as $t) {
+            array_push($postTagArray, $t);
+        }
+    }
+    $postTagArray = array_unique($postTagArray);
+    $smarty->assign('postTagArray', $postTagArray);
+    $smarty->assign('postArray', $postArray);
+    $smarty->assign('title', $defaultTitle);
+    $smarty->assign('description', $defaultDescription);
 }
 
 $smarty->assign('postHideState', $postHideState);
 $smarty->assign('showPostListing', $showPostListing);
-
 ?>

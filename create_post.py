895a3d697aae2e379142af2fd402c02cb1e771c5
kate <kate@dariox.club>
2024-05-23 12:22:40 +0800

    Rewrite logic in create_post.py for reusable functions

--- create_post.py
+++ create_post.py
@@ -3,40 +3,97 @@ from datetime import datetime
 
 now = datetime.now()
 
-print('PostId name (default: YYYYMMDD)')
-name = input('> ')
-if len(name) < 1:
-    name = now.strftime('%Y%m%d')
-print('name: %s\n' % name)
+def format_title(title, required, extra=None):
+    if title is not None:
+        title_str = title
+        if required:
+            if extra is None:
+                title_str += ' (required)'
+            else:
+                title_str += ' (required, ' + str(extra) + ')'
+        else:
+            if extra is not None:
+                title_str += ' (' + str(extra) + ')'
+        return title_str
+    else:
+        return None
+def print_format_title(title, required, extra=None):
+    s = format_title(title, required, extra)
+    if s is not None:
+        print(s)
+def get_string(
+        title='My thing!',
+        required=False,
+        defaultValue=None):
+    defaultValue_title = None
+    if defaultValue is not None:
+        defaultValue_title = 'default: ' + str(defaultValue)
+    print_format_title(title, required, defaultValue_title)
 
-def get_subject():
-    print('Subject (required)')
+
     s = input('> ')
-    if len(s) < 1:
-        print('--- Subject IS required ---')
-        return get_subject()
+    if len(s.strip()) < 1:
+        if required:
+            print('--- Value is required! ---')
+            return get_string(title=None, required=required, defaultValue=defaultValue)
+        else:
+            print('Using default value')
+            return defaultValue
     else:
         return s
-subject = get_subject()
+def get_enum_value_single(
+    title='My enum',
+    required=False,
+    defaultIndex=None,
+    possibleValues=[],
+    possibleValuesDesc=[]):
 
-def get_hide_state():
-    print('Hide state (0: show, 1: does not exist, 2: access via url only)')
-    h = str(input('> '))
-    if h != '0' and h != '1' and h != 2:
-        print('--- Hide state IS required ---')
-        return get_hide_state()
+    print_format_title(title, required)
+
+    for i in range(len(possibleValues)):
+        ps = str(possibleValues[i]) + ': ' + str(possibleValuesDesc[i])
+        if i == defaultIndex:
+            ps += ' (default)'
+        print(ps)
+
+
+    s = input('> ')
+    if len(s.strip()) < 1:
+        if required:
+            print('--- Value is required! ---')
+            return get_enum_value_single(title, required, defaultIndex, possibleValues, possibleValuesDesc)
+        else:
+            if defaultIndex is None:
+                return None
+            else:
+                return possibleValues[defaultIndex]
     else:
-        return h
-hide_state = get_hide_state()
+        for x in possibleValues:
+            if x == s:
+                return x
+        print('Could not find value %s. Try again please' % s)
+        return get_enum_value_single(title, required, defaultIndex, possibleValues, possibleValuesDesc)
 
-def get_tags():
-    print('Tags (comma seperated)')
-    h = str(input('>')).split(',')
+def get_string_arr(
+        title='My string array',
+        required=False):
+    if title is not None:
+        title += ' (comma separated)'
+    print_format_title(title, required)
+    s = str(input('> ')).split(',')
     resultArr = []
-    for item in h:
+    for item in s:
         resultArr.append("'" + item.strip() + "'")
-    return ', '.join(resultArr)
-post_tags = get_tags()
+    return resultArr
+
+name = get_string('PostId name', required=False, defaultValue=str(now.strftime('%Y%m%d')))
+subject = get_string('Subject', required=True)
+hide_state = get_enum_value_single('Hide state', required=True, defaultIndex=None, possibleValues=['0', '1', '2'], possibleValuesDesc=[
+    'Show post',
+    'Mark as deleted',
+    'Hidden. Can only be access via URL'
+])
+post_tags = ','.join(get_string_arr('Tags', required=False))
 
 _target_basename = './blog_posts/%s' % name
 
@@ -59,4 +116,3 @@ fphp.close()
 fmd = open('%s.md' % _target_basename, 'x')
 fmd.write('')
 fmd.close()
-    

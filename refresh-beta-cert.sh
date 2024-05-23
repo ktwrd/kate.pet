8c4e6c11dcc582ea3420b4f3ca05bce46b210aae
kate <kate@dariox.club>
2024-05-23 12:22:19 +0800

    Use variables from .env in refresh-beta-cert.sh

--- refresh-beta-cert.sh
+++ refresh-beta-cert.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-certbot certonly --nginx -d beta.kate.pet --staple-ocsp -m kate@dariox.club --agree-tos
+if [ ! -f ".env" ]; then
+    echo "no .env file found!"
+    exit 1
+fi
+
+while read -r LINE; do
+    if [[ $LINE == *'='* ]] && [[ $LINE != '#'* ]]; then
+        ENV_VAR="$(echo $LINE | envsubst)"
+        eval "declare $ENV_VAR"
+    fi
+done < .env
+
+
+certbot certonly --nginx -d $NGINX_DOMAIN --staple-ocsp -m $CERT_EMAIL --agree-tos

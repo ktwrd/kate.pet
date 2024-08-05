6fc8a7750bea4c0b7c97c3ed058ddfe23efcd650
kate <kate@dariox.club>
2024-08-05 19:13:16 +0800

    Add new blog post

---
+++ blog_posts/deadlock.php
@@ -0,0 +1,11 @@
+<?php
+
+$generate_blog_post = array(
+    'subject' => 'Deadlock',
+    'description' => 'So VALVe has released a new game.',
+    'created_at' => mktime(19, 15, 0, 8, 5, 2024),
+    'hide_state' => 0,
+    'tags' => array('VALVe','Gaming','Rant')
+);
+
+?>

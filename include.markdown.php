00eb46ae61aee9c61c6176d4e41c46588d34cc3e
kate <kate@dariox.club>
2024-11-14 11:59:25 +0800

    [include.markdown] Add table support

--- include.markdown.php
+++ include.markdown.php
@@ -3,6 +3,7 @@ use League\CommonMark\Environment\Environment;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 use League\CommonMark\Extension\CommonMark\Node\Block\FencedCode;
 use League\CommonMark\Extension\CommonMark\Node\Block\IndentedCode;
+use League\CommonMark\Extension\Table\TableExtension;
 use League\CommonMark\MarkdownConverter;
 use Spatie\CommonMarkHighlighter\FencedCodeRenderer;
 use Spatie\CommonMarkHighlighter\IndentedCodeRenderer;
@@ -11,8 +12,18 @@ if (!function_exists('formatMarkdown'))
 {
     function formatMarkdown($text)
     {
-        $environment = new Environment();
+        $config = [
+            'table' => [
+                'wrap' => [
+                    'enabled' => true,
+                    'tag' => 'div',
+                    'attributes' => ['class' => 'table-responsive'],
+                ]
+            ],
+        ];
+        $environment = new Environment($config);
         $environment->addExtension(new CommonMarkCoreExtension());
+        $environment->addExtension(new TableExtension());
         $environment->addRenderer(FencedCode::class, new FencedCodeRenderer(['html', 'js', 'css', 'bash', 'php', 'json']));
         $environment->addRenderer(IndentedCode::class, new IndentedCodeRenderer(['html', 'js', 'css', 'bash', 'php', 'json']));
     

dd92738237b5e3be38ec2969c9c39301edda2d09
kate <kate@dariox.club>
2025-01-26 15:49:11 +0800

    [blog system] Added support for comments via utteranc.es

--- blog_posts/adastral-pt1.php
+++ blog_posts/adastral-pt1.php
@@ -5,7 +5,13 @@ $generate_blog_post = array(
     'description' => 'Something fast has been brewing...',
     'created_at' => mktime(13, 0, 0, 01, 26, 2025),
     'hide_state' => 0,
-    'tags' => array('Development','Adastral')
+    'tags' => array('Development','Adastral'),
+    'comment_cfg' => array(
+        'system' => 'utterances',
+        'issue_number' => '1',
+        'repo' => 'ktwrd/kate.pet',
+        'label' => 'comment section - blog post'
+    )
 );
 
 ?>

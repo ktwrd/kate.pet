5c2ec59f46e198d620d6ce4ead11a61f4f95747d
kate <kate@dariox.club>
2025-01-26 13:07:32 +0800

    Updated adastral-pt1.php

--- blog_posts/adastral-pt1.php
+++ blog_posts/adastral-pt1.php
@@ -4,7 +4,7 @@ $generate_blog_post = array(
     'subject' => 'Working with Adastral',
     'description' => 'Something fast has been brewing...',
     'created_at' => mktime(13, 0, 0, 01, 26, 2025),
-    'hide_state' => 2,
+    'hide_state' => 0,
     'tags' => array('Development','Adastral')
 );
 

78fa274ab7986eafc5a479d0948e51e19e3af271
kate <kate@dariox.club>
2025-02-15 09:59:39 +0800

    Rewrite logic & how data is structured for links list

--- pages/links.php
+++ pages/links.php
@@ -1,81 +1,143 @@
 <?php
 $linkData = array(
-    array(
-        'label' => 'Mastodon',
-        'link' => 'https://dariox.club/@kate',
-        'short' => 'mastodon'
-    ),
-    array(
-        'label' => 'Lemmy',
-        'link' => 'https://lemmy.blahaj.zone/u/ktwrd',
-        'short' => 'lemmy'
-    ),
-    array(
-        'label' => 'Bluesky',
-        'link' => 'https://bsky.app/profile/kate.pet',
-        'text' => '@kate.pet',
-        'short' => 'bluesky'
-    ),
-    array(
-        'label' => 'Blog',
-        'link' => 'https://kate.pet/p/blog',
-        'short' => 'blog'
-    ),
-    array(
-        'label' => 'Twitter',
-        'link' => 'https://twitter.com/seedvevo',
-        'short' => 'twitter'
-    ),
-    array(
-        'label' => '4chan',
-        'text' => 'kate !!DI0PM78d0GV',
-    ),
-    array(
-        'label' => 'Email',
-        'text' => 'kate@dariox.club',
-    ),
-    // array(
-    //     'label' => 'XMPP',
-    //     'text' => 'kate@redfur.cloud'
-    // ),
-    array(
-        'label' => 'Pronouns',
-        'link' => 'https://en.pronouns.page/@seedvevo',
-        'short' => 'pronouns'
-    ),
-    array(
-        'label' => 'Discord Server',
-        'link' => 'https://discord.gg/PMrqTQPZFE',
-        'short' => 'discord'
-    ),
-    array(
-        'label' => 'Github',
-        'link' => 'https://github.com/ktwrd',
-        'short' => 'github'
-    ),
-    array(
-        'label' => 'Last.fm',
-        'link' => 'https://last.fm/user/seedvevo',
-        'short' => 'lastfm'
-    ),
-    array(
-        'label' => 'Steam',
-        'link' => 'https://steamcommunity.com/id/kate_main',
-        'short' => 'steam'
-    ),
-    array(
-        'label' => 'Ko-fi (Donate)',
-        'link' => 'https://ko-fi.com/ktwrd',
-        'short' => 'donate'
-    )
+    [
+        'name' => 'Social Media',
+        'id' => 'social',
+        'items' => [
+            [
+                'name' => 'Bluesky',
+                'id' => 'bluesky',
+                'link' => 'https://bsky.app/profile/kate.pet',
+                'link_alt' => ['bluesky', 'bsky'],
+                'link_txt' => '@kate.pet'
+            ],
+            [
+                'name' => 'Mastodon',
+                'id' => 'mastodon',
+                'link' => 'https://dariox.club/@kate',
+                'link_txt' => '@kate@dariox.club'
+            ],
+            [
+                'name' => 'Twitter',
+                'id' => 'twitter',
+                'link' => 'https://twitter.com/seedvevo',
+                'link_alt' => ['twitter', 'twt', 'x'],
+                'link_txt' => '@SeedVEVO'
+            ],
+            [
+                'name' => 'Reddit',
+                'id' => 'reddit',
+                'link' => 'https://reddit.com/u/ktwrd',
+                'link_txt' => 'u/ktwrd'
+            ],
+            [
+                'name' => '4chan',
+                'txt' => 'kate !!DI0PM78d0GV'
+            ]
+        ]
+    ],
+    [
+        'name' => 'Development',
+        'id' => 'dev',
+        'items' => [
+            [
+                'name' => 'Github',
+                'id' => 'github',
+                'link' => 'https://github.com/ktwrd'
+            ],
+            [
+                'name' => 'Gitlab',
+                'id' => 'gitlab',
+                'link' => 'https://gitlab.com/ktwrd'
+            ]
+        ]
+    ],
+    [
+        'name' => 'Projects',
+        'id' => 'projects',
+        'items' => [
+            [
+                'name' => 'Xenia Bot',
+                'id' => 'xeniabot',
+                'link' => 'https://xenia.kate.pet'
+            ],
+            [
+                'name' => 'Uptime',
+                'id' => 'uptime',
+                'link' => 'https://uptime.dxcdn.net/status/kate'
+            ],
+            [
+                'name' => 'Sixgrid',
+                'id' => 'sixgrid',
+                'link' => 'https://sixgrid.kate.pet'
+            ]
+        ]
+    ],
+    [
+        'name' => 'Other',
+        'id' => 'other',
+        'items' => [
+            [
+                'name' => 'Ko-fi (Donate)',
+                'id' => 'donate',
+                'link' => 'https://ko-fi.com/ktwrd'
+            ],
+            [
+                'name' => 'last.fm',
+                'id' => 'lastfm',
+                'link' => 'https://last.fm/user/seedvevo'
+            ],
+            [
+                'name' => 'Steam',
+                'id' => 'steam',
+                'link' => 'https://steamcommunity.com/id/kate_main'
+            ],
+            [
+                'name' => 'Discord Server',
+                'id' => 'discord',
+                'link' => 'https://discord.gg/PMrqTQPZFE'
+            ],
+            // [
+            //     'name' => 'Pronouns',
+            //     'id' => 'pronouns',
+            //     'link' => 'https://en.pronouns.page/@seedvevo'
+            // ],
+            [
+                'name' => 'Email',
+                'id' => 'email',
+                'link' => 'mailto:kate@dariox.club',
+                'link_txt' => 'kate@dariox.club'
+            ]
+        ]
+    ]
 );
+
 $smarty->assign('pageLinks', $linkData);
+// $smarty->assign('pageLinks', $linkData);
 
 $found = 0;
 if (isset($_REQUEST['i']))
 {
-    foreach ($linkData as $link)
+    foreach ($linkData as $linkGroup)
     {
+        foreach ($linkGroup['items'] as $linkItem) {
+            if ($found == 1) {
+                continue;
+            }
+            if (isset($linkItem['link']) && isset($linkItem['id'])) {
+                if ($linkItem[id] == $_REQUEST['i']) {
+                    header('Location: ' . $linkItem['link']);
+                    $found = 1;
+                } else {
+                    if (isset($linkItem['link_alt'])) {
+                        if (in_array($_REQUEST['i'], $linkItem['link_alt'])) {
+                            header('Location: ' . $linkItem['link']);
+                            $found = 1;
+                        }
+                    }
+                }
+            }
+        }
         if (isset($link['short']))
         {
             if ($link['short'] == $_REQUEST['i'])

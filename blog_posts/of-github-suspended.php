07f6b7308104c379c862cff259f71a1abd2f658c
kate <kate@dariox.club>
2025-05-09 10:04:20 +0800

    Updated blog_posts/of-github-suspended.php

--- blog_posts/of-github-suspended.php
+++ blog_posts/of-github-suspended.php
@@ -5,7 +5,13 @@ $generate_blog_post = array(
     'description' => 'The Open Fortress GitHub was suspended?????',
     'created_at' => mktime(16, 37, 3, 5, 8, 2025),
     'hide_state' => 0,
-    'tags' => array('Development','Open Fortress','GitHub')
+    'tags' => array('Development','Open Fortress','GitHub'),
+    'comment_cfg' => array(
+        'system' => 'utterances',
+        'issue_number' => '3',
+        'repo' => 'ktwrd/kate.pet',
+        'label' => 'comment section - blog post'
+    )
 );
 
 ?>

3848df17ecfa73b2f56b6c3987a1736891bd6b9c
kate <kate@dariox.club>
2025-09-17 21:04:50 +0800

    Updated desc

--- blog_posts/open-fortress.php
+++ blog_posts/open-fortress.php
@@ -2,7 +2,8 @@
 
 $generate_blog_post = array(
     'subject' => 'Finally out of Alpha',
-    'description' => 'Welcome to Open Fortress. After nearly a year of development, and 5 years since release, hopefully it would have been worth the wait.',
+    // TODO update "X years since release" to be accurate when release date is announced
+    'description' => 'Welcome to Open Fortress. After nearly a year of development, and 6 years since release, hopefully it would have been worth the wait.',
     'created_at' => mktime(0, 0, 0, 10, 1, 2025), // TODO write blog post and update to date-of-publish
     'hide_state' => 1,
     'tags' => array('Development','Open Fortress', 'Get Psyched! Games'),

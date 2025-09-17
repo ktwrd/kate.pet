9cdafff76ecee816431aa63006919958f95cc0f4
kate <kate@dariox.club>
2025-09-17 21:02:07 +0800

    Hid blog post

--- blog_posts/open-fortress.php
+++ blog_posts/open-fortress.php
@@ -4,7 +4,7 @@ $generate_blog_post = array(
     'subject' => 'Finally out of Alpha',
     'description' => 'Welcome to Open Fortress. After nearly a year of development, and 5 years since release, hopefully it would have been worth the wait.',
     'created_at' => mktime(0, 0, 0, 10, 1, 2025), // TODO write blog post and update to date-of-publish
-    'hide_state' => 2,
+    'hide_state' => 1,
     'tags' => array('Development','Open Fortress', 'Get Psyched! Games'),
 );
 
